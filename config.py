04f9aac597d9e05d8ef6cf8b22813379a97d78d7
Jana Dolan <jana.dolan@hs-mainz.de>
2019-03-10 16:10:22 +0100

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-from os.path import join, dirname, realpath
-
-class Config(object):
-    SECRET_KEY = 'dev'
-    UPLOAD_FOLDER = join(dirname(realpath(__file__)), 'app/static/uploads')
-    ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'md', 'pdf', 'png', 'tex' ])
