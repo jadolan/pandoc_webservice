626094303e3e8111e3f7ee24b054c5e8cd60b529
Jana Dolan <jdolan@students.uni-mainz.de>
2019-03-10 16:13:52 +0100

    change template structure

--- app/routes.py
+++ app/routes.py
@@ -7,14 +7,20 @@ import os
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
+    return render_template('upload.html', form=UploadForm())
+    
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    loaded = False
     form = UploadForm()
     # check if POST-request containing a file has been sent
     if form.validate_on_submit():
         filename = secure_filename(form.file.data.filename)
         form.file.data.save(os.path.join(
             app.config['UPLOAD_FOLDER'], filename))
-    return render_template('index.html', form=form)
-
+        loaded = True
+    return render_template('upload.html', form=form)
 
 
 @app.route('/login')
